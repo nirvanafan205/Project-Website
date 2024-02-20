0d03e9baefc5583d5b902b357aa458f9ba05b821
captain.peanutbutter495 <47827017+nirvanafan205@users.noreply.github.com>
2024-02-19 22:27:29 -0800

    scratched the carousel idea

--- src/Components/ProjectInfo.jsx
+++ src/Components/ProjectInfo.jsx
@@ -7,49 +7,23 @@ import figmaIcon from "@iconify-icons/logos/figma";
 import reactIcon from "@iconify-icons/logos/react";
 import firebaseIcon from "@iconify-icons/logos/firebase";
 import reduxIcon from "@iconify-icons/logos/redux";
-import "../style.css";
-
-const initialIcons = [
-  githubIcon,
-  kotlinIcon,
-  jiraIcon,
-  figmaIcon,
-  reactIcon,
-  firebaseIcon,
-  reduxIcon,
-];
 
 const ProjectInfo = () => {
-  const [icons, setIcons] = useState(initialIcons);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIcons((prevIcons) => {
-        // Move the first icon to the end to create an infinite loop
-        return [...prevIcons.slice(1), prevIcons[0]];
-      });
-    }, 2000); // 2000ms interval for each icon move
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <div>
       {/* Existing sections... */}
 
       {/* Technologies and Skills Section */}
-      <section className="tw-flex tw-flex-col tw-items-center tw-justify-center tw-bg-pink-100 tw-py-8">
-        <h2 className="tw-text-2xl tw-mb-4">Technologies and Skills</h2>
-        <div className="carousel-container tw-relative tw-w-full tw-flex tw-justify-center">
-          <div className="sliding-container tw-flex">
-            {icons.map((icon, index) => (
-              <Icon
-                key={index}
-                className="icon tw-text-6xl tw-mx-4" // Adjust margins as necessary
-                icon={icon}
-              />
-            ))}
-          </div>
+      <section className="tw-bg-pink-100 tw-flex tw-flex-col tw-justify-center tw-items-center">
+        <h2>Technologies and Skills</h2>
+        <div className="tw-flex tw-pt-4 tw-pb-4 tw-flex-wrap">
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={githubIcon} />
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={kotlinIcon} />
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={jiraIcon} />
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={figmaIcon} />
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={reactIcon} />
+          <Icon className="tw-text-6xl tw-mr-8 tw-mb-4" icon={firebaseIcon} />
+          <Icon className="tw-text-6xl tw-mb-4" icon={reduxIcon} />
         </div>
       </section>
     </div>
